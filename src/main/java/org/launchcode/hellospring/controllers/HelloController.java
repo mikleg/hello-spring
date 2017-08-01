756f5b9179702d908e58772fa3c8c811fed2c7fa
Mikle Gorbunov <mihailgorbunov74@gmail.com>
2017-07-31 18:46:15 -0700

    the second video prep 3.3

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,19 +3,45 @@ package org.launchcode.hellospring.controllers;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @org.springframework.stereotype.Controller
+
 public class HelloController
 {
     @RequestMapping(value = "")
     @ResponseBody
-    public String index()
+    public String index(HttpServletRequest request)
     {
-        return "Hello World!";
+        String userName = request.getParameter("name");
+        if (userName == null)
+        {userName = "world";}
+        return "Hello " + userName + "!";
     }
-    @RequestMapping(value = "Goodbye")
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloForm(){
+        String html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!'/>" +
+                "</form>";
+        return html;
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request){
+        String name = request.getParameter("name");
+
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "goodbye")
     @ResponseBody
     public String goodbye()
     {
